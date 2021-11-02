f78b12ac64c1d6a5a8e7edce3a99d7a4d2d033a9
TlabAhmet <93315535+TlabAhmet@users.noreply.github.com>
2021-11-02 11:37:06 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Part of Softhealer Technologies.
+{
+    "name": "Partner Incoming Outgoing Emails | Incoming Emails in partner | Outgoing Emails in customer | Incoming Mails in vendor | Outgoing Emails in supplier | Incoming Emails in user",
+    
+    "author": "Softhealer Technologies",
+    
+    "website": "https://www.softhealer.com",    
+    
+    "support": "support@softhealer.com",   
+    
+    "version": "11.0.1",
+    
+    "category": "Discuss",
+    
+    "summary": "User Outgoing Emails,Mail by customer module, partner filter emails, vendor Outgoing mail app, filter incoming mails,person outgoing mail, Employee Incoming Mails odoo",   
+    
+    "description": """Partner Incoming Outgoing Emails. Easy to check incoming and outgoing emails in customers, vendors or any other contacts.""", 
+    
+    "depends": ['contacts','mail'],
+    
+    "data": [
+                'views/res_partner.xml',
+            ],    
+    
+    "images": ["static/description/background.png",],             
+    
+    "installable": True,
+    "auto_install": False,
+    "application": True,       
+    
+    "price": "15",
+    "currency": "EUR",          
+}
